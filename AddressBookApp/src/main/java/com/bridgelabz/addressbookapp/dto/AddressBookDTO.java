8b0be151d05b70b8ca9394ab784ad4b4f3a43199
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-25 22:53:17 +0530

    Added:To handle errors and provide user friendly response

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
@@ -8,16 +9,16 @@ import lombok.Data;
 public @Data class AddressBookDTO {
 
 
-	@Pattern(regexp = "^[A-Z]{1}[a-z A-Z\\s]{2,}$", message = "Employee Name Invalid")
-	@NotEmpty(message = "Employee name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-z A-Z\\s]{2,}$", message = "Person FirstName Invalid")
+	@NotEmpty(message = "FirstName cannot be null")
 	public String firstName;
 	
 
-	@Pattern(regexp = "^[A-Z]{1}[a-z A-Z\\s]{2,}$", message = "Employee Name Invalid")
-	@NotEmpty(message = "Employee name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-z A-Z\\s]{2,}$", message = "Person LastName Invalid")
+	@NotEmpty(message = "LastName cannot be null")
 	public String lastName;
 	
-	@NotEmpty(message = "Employee name cannot be null")
+	@NotBlank(message = "Address cannot be null")
 	public String address;
 	
 	public AddressBookDTO() { }
